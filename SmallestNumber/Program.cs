4382ea783429dbe8e320a6c2e0681ef88efd2047
Kamnen Minkov <kamenminkov.com@gmail.com>
2022-05-15 23:47:02 +0300

    add smllest number solution

--- SmallestNumber/Program.cs
+++ SmallestNumber/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace SmallestNumber
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmallestNumber
 {
     internal class Program
     {
         static void Main()
         {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            Console.WriteLine(GetSmallest(numbers));
+        }
+
+        public static int GetSmallest(List<int> numbers)
+        {
+            if (numbers.Count == 0)
+            {
+                throw new ArgumentException("The list of should contain at list one element");
+            }
+
+            int possibleMin1 = numbers.First();
+
+            if (numbers.Count == 1)
+            {
+                return possibleMin1;
+            }
+
+            int possibleMin2 = GetSmallest(numbers.Skip(1).ToList());
+
+            return possibleMin1 < possibleMin2 ? possibleMin1 : possibleMin2;
         }
     }
 }
